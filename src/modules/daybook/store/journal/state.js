4b1d70b781335acc7dc386d08e89510209d83a07
yeferson castiblanco <yeferson.castiblanco@inoutdelivery.com>
2023-06-05 13:39:57 -0500

    todo funcional antes del compotions api

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,29 @@
 export default () => ({
-  isloding : true,
-  entries:[1,2,3,4,5,6,7,8,9]
+  isloding: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'haskjdhasjkdhaskjlhdni uhiuwiyeiun ehdowmd asudoiausoe idasmjdbnaslk jd hiny ',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 100,
+      date: new Date().toDateString(),
+      text: 'Consectetur culpa sit commodo anim dolore ullamco deserunt. Dolore non consequat tempor in pariatur cillum. Ad sunt eu incididunt consequat occaecat do ea adipisicing excepteur. Fugiat quis enim duis eiusmod labore quis. Laboris nostrud sint enim laborum cillum consequat.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 100,
+      date: new Date().toDateString(),
+      text: 'Sint elit aliqua voluptate commodo mollit dolor incididunt ut. Do sit officia esse voluptate elit pariatur minim eu ut aliquip enim consequat aute. Elit ullamco est ullamco excepteur eiusmod excepteur esse laboris voluptate. Adipisicing deserunt eu excepteur veniam culpa officia eiusmod deserunt consectetur enim velit ut commodo. Laborum labore et sunt esse excepteur cillum occaecat minim aute velit consectetur quis nulla commodo. Duis aliqua aliquip ullamco adipisicing ea aliqua sit ad occaecat adipisicing duis duis.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 100,
+      date: new Date().toDateString(),
+      text: 'Minim elit officia incididunt et nulla quis est ut nulla nulla laborum esse. Do mollit amet excepteur nisi aute ipsum cupidatat dolore. Amet commodo occaecat nisi mollit aliqua incididunt dolore tempor sint voluptate eiusmod ea anim excepteur. Irure sint mollit incididunt reprehenderit adipisicing ad.',
+      picture: null,
+    },
+  ],
 })
